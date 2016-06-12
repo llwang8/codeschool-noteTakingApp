3d610362bc60004904710d573c28ae3fe2d855ab
llwang8 <wangxbl56@gmail.com>
2016-06-11 22:56:02 -0400

    add categories select options

--- app/assets/javascript/controllers/notesEditController.js
+++ app/assets/javascript/controllers/notesEditController.js
@@ -1,7 +1,8 @@
 angular.module('NoteWrangler')
-.controller('NotesEditController', function(Note, $scope, $routeParams, $location){
+.controller('NotesEditController', function(Note, $scope, category, $routeParams, $location){
     $scope.note = Note.get({id: $routeParams.id});
     $scope.isSubmitting = false;
+    $scope.categories = Category.query();
 
     $scope.saveNote = function(note){
         $scope.isSubmitting = true;
